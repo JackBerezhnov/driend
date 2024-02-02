720cb324d180a56fbf33d84021642b078f602f07
Evgeny Berezhnov <33531079+JackBerezhnov@users.noreply.github.com>
2024-02-01 21:07:36 -0800

    Added Material UI library

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,10 @@
+import Button from '@mui/material/Button';
+
 export default function Home() {
   return (
     <div>
       <h1>Hello World</h1>
+      <Button variant="outlined">Outlined</Button>
     </div>
   );
 }
