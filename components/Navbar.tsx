966992ae2e245ee382afee34dc7beb4dfc5a0664
Evgeny Berezhnov <33531079+JackBerezhnov@users.noreply.github.com>
2024-02-04 20:33:09 -0800

    Created simple navbar

---
+++ components/Navbar.tsx
@@ -0,0 +1,11 @@
+const Navbar = () => {
+    return(
+        <nav>
+            <a>Home</a>
+            <a>About</a>
+            <a>Forum</a>
+        </nav>
+    )
+}
+
+export default Navbar;
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,8 +1,10 @@
 import Button from '@mui/material/Button';
+import Navbar from '../../components/Navbar';
 
 export default function Home() {
   return (
     <div>
+      <Navbar />
       <h1>Hello World</h1>
       <Button variant="outlined">Outlined</Button>
     </div>
