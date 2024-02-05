9a8b56f0015d76a79e62645f02e0abc2416a38ff
Evgeny Berezhnov <33531079+JackBerezhnov@users.noreply.github.com>
2024-02-04 21:01:09 -0800

    Changed text for h1

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -7,7 +7,7 @@ export default function Home() {
   return (
     <div>
       <Navbar />
-      <h1>Hello World</h1>
+      <h1>Driend</h1>
       <Button variant="outlined">Outlined</Button>
       <TextField id="outlined-basic" label="Put your text here" variant="outlined"/>
       <Footer />
