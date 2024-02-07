b5ac5bb0e9d6e16a54e3bd88435db11205696c93
Evgeny Berezhnov <33531079+JackBerezhnov@users.noreply.github.com>
2024-02-06 22:24:31 -0800

    Wrote getData function for fetch data

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,13 +3,26 @@ import Footer from '../../components/Footer';
 import Navbar from '../../components/Navbar';
 import TextField from '@mui/material/TextField';
 
-export default function Home() {
+async function getData() {
+  const res = await fetch("https://catfact.ninja/breeds");
+
+  if(!res.ok) {
+    throw new Error('Failed to fetch data');
+  }
+
+  return res.json();
+}
+
+export default async function Home() {
+  const data = await getData();
+
   return (
     <div className="flex flex-col justify-center items-center text-center bg-gray-200 h-screen">
       <Navbar />
       <h1 className="text-black">Driend</h1>
       <TextField id="outlined-basic" label="Put your text here" variant="outlined" className="bg-gray-200 text-white"/>
       <Button variant="contained">Post</Button>
+      <p>{data.data[0].breed}</p>
       <Footer />
     </div>
   );
