d379da1793443043c032cabaa07223552fa98b0d
Evgeny Berezhnov <33531079+JackBerezhnov@users.noreply.github.com>
2024-02-07 21:52:59 -0800

    Added handleInput function

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -4,6 +4,7 @@ import Button from '@mui/material/Button';
 import Footer from '../../components/Footer';
 import Navbar from '../../components/Navbar';
 import TextField from '@mui/material/TextField';
+import React, { useState } from 'react';
 
 async function getData() {
   const res = await fetch("https://catfact.ninja/breeds");
@@ -16,17 +17,25 @@ async function getData() {
 }
 
 export default async function Home() {
+  const [input, setInput] = useState('');
+
+
   const data = await getData();
 
   function handlePost() {
     console.log("Hello");
   }
 
+  function handleInput(event: React.ChangeEvent<HTMLInputElement>) {
+    event.preventDefault();
+    console.log(event.target.value);
+  }
+
   return (
     <div className="flex flex-col justify-center items-center text-center bg-gray-200 h-screen">
       <Navbar />
       <h1 className="text-black">Driend</h1>
-      <TextField id="outlined-basic" label="Put your text here" variant="outlined" className="bg-gray-200 text-white"/>
+      <TextField onChange={handleInput} id="outlined-basic" label="Put your text here" variant="outlined" className="bg-gray-200 text-white"/>
       <Button onClick={handlePost} variant="contained">Post</Button>
       <p>{data.data[0].breed}</p>
       <Footer />
